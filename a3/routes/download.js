98e3d8bbc23d20ea11f8a80b29f9219b7b7329bc
p4rq <121601351+p4rq@users.noreply.github.com>
2024-02-11 13:53:47 +0600

    Update download.js

--- a3/routes/download.js
+++ a3/routes/download.js
@@ -5,26 +5,22 @@ const pdf = require('html-pdf');
 const ExchangeRate = require('../models/exchangeRate');
 const News = require('../models/news');
 
-// Route to download historical exchange rate data as PDF
 router.get('/news', async (req, res) => {
     try {
       const newsData = await News.find().sort({ timestamp: 'desc' });
   
-      // Render EJS template for news data
       res.render('download/news', { newsData }, (err, html) => {
         if (err) {
           console.error(err);
           return res.status(500).send('Internal Server Error');
         }
   
-        // Generate PDF
         pdf.create(html).toBuffer((err, buffer) => {
           if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
           }
   
-          // Send PDF as response
           res.setHeader('Content-Type', 'application/pdf');
           res.setHeader('Content-Disposition', 'attachment; filename=newsData.pdf');
           res.send(buffer);
@@ -38,7 +34,6 @@ router.get('/news', async (req, res) => {
   
   module.exports = router;
 
-// Route to download historical news data as PDF
 router.get('/news', async (req, res) => {
   try {
     const newsData = await News.find().sort({ timestamp: 'desc' });
