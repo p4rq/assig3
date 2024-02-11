dd785a75a156695dfce061cd87f61ae410eba7e3
p4rq <121601351+p4rq@users.noreply.github.com>
2024-02-11 13:57:06 +0600

    Update register.js

--- a3/routes/register.js
+++ a3/routes/register.js
@@ -3,25 +3,21 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
-const uuid = require('uuid'); // Добавлен импорт для использования библиотеки uuid
+const uuid = require('uuid');
 
-// Определение функции генерации ID пользователя
 const generateUserId = () => {
   return uuid.v4();
 };
 
-// Роут для отображения страницы регистрации
 router.get('', (req, res) => {
-  res.render('../views/register.ejs', { error: null }); // Определение 'error' как null изначально
+  res.render('../views/register.ejs', { error: null }); 
 });
 
-// Роут для обработки данных формы регистрации
 router.post('/', [
   body('username').notEmpty().isString(),
   body('password').notEmpty().isString(),
 ], async (req, res) => {
   try {
-    // Проверка наличия ошибок валидации
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.render('../views/register.ejs', { error: 'Invalid username or password' });
@@ -29,20 +25,18 @@ router.post('/', [
 
     const { username, password } = req.body;
 
-    // Хэширование пароля перед сохранением в базу данных
     const hashedPassword = bcrypt.hashSync(password, 10);
 
-    // Создание нового пользователя
     const newUser = new User({
       username,
       password: hashedPassword,
-      userId: generateUserId(), // Использование функции generateUserId
+      userId: generateUserId(),
       creationDate: new Date(),
       isAdmin: false,
     });
 
-    await newUser.save(); // Сохранение пользователя в базе данных
-    res.redirect('/login'); // Перенаправление на страницу входа после успешной регистрации
+    await newUser.save();
+    res.redirect('/login'); 
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
