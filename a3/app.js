0fa54cfecb46eb76c5a2b5424ab8bddf1ab21322
p4rq <121601351+p4rq@users.noreply.github.com>
2024-02-11 13:59:52 +0600

    Update app.js

--- a3/app.js
+++ a3/app.js
@@ -5,15 +5,14 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 const helmet = require('helmet');
 const cors = require('cors');
-const path = require('path'); // Add path module for file paths
+const path = require('path');
 const winston = require('winston');
-//const chartRoute = require('./routes/chart'); // Adjust the route based on your project structure
+//const chartRoute = require('./routes/chart');
 //const i18next = require('./i18n');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 //mongodb+srv://anuarbek0471:9BQm7QWmRsvtfoKI@cluster0.xycxadw.mongodb.net/?retryWrites=true&w=majority"
-// MongoDB connection
 mongoose.connect("mongodb+srv://anuarbek0471:9BQm7QWmRsvtfoKI@cluster0.xycxadw.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -22,11 +21,10 @@ db.once('open', () => console.log('Connected to MongoDB'));
 //app.use('/chart', chartRoute); // Adjust the route based on your project structure
 
 
-// Middleware
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(session({ secret: 'ba686a76b6c89f97646df8c0b98b7a7f1408523aef8fffe7d2a9cce8c4297e40', resave: true, saveUninitialized: true }));
@@ -38,8 +36,8 @@ app.use('/register', registerRoute);
 app.use('/', require('./routes/main'));
 app.use('/login', require('./routes/login'));
 app.use('/admin', require('./routes/admin'));
-const mainRouter = require('./routes/index'); // или путь к вашему файлу с маршрутами
-app.use('/', mainRouter); // здесь используется корневой путь, вы можете изменить его по необходимости
+const mainRouter = require('./routes/index');
+app.use('/', mainRouter);
 app.use('/exchangeRate', require('./routes/main'));
 //app.use('/', chartRoute);
 const downloadRoute = require('./routes/download');
